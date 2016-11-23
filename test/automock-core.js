fadc55bb80ecc222d3a6bf764cd6d76cd6541df5
Ersun Warncke <ersun.warncke@gmail.com>
2016-11-23 06:37:28 -0400

    split core tests into own file, always reset automock

---
+++ test/automock-core.js
@@ -0,0 +1,138 @@
+'use strict'
+
+const MockLogClient = require('../mock/mock-log-client')
+const Promise = require('bluebird')
+const _ = require('lodash')
+const automock = require('../lib/immutable-automock')
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+const immutable = require('immutable-core')
+
+chai.use(chaiAsPromised)
+const assert = chai.assert
+
+describe('immutable-automock: core', function () {
+
+    it('should wrap method with automock function', function () {
+        // reset global singleton data
+        immutable.reset()
+        // apply automock
+        automock.reset().mock()
+        // create FooModule
+        var fooModule = immutable.module('FooModule', {
+            // foo method returns valid Promise
+            foo: function (args) {
+                return Promise.resolve(true)
+            },
+        })
+        // check that foo is wrapped with automock function
+        assert.match(fooModule.foo.toString(), /automockWrapper/)
+    })
+
+    it('should call original method when no mock data is loaded', function () {
+        // reset global singleton data
+        immutable.reset().strictArgs(false)
+        // apply automock
+        automock.reset().mock()
+        // will be set true when method called
+        var methodCalled = false
+        // create FooModule
+        var fooModule = immutable.module('FooModule', {
+            // foo method returns valid Promise
+            foo: function (args) {
+                methodCalled = true
+            },
+        })
+        // call method
+        return fooModule.foo().then(function () {
+            // check that original method was called
+            assert.isTrue(methodCalled)
+        })
+    })
+
+    it('should return mock data and not call original method when mock data loaded', function () {
+        // build mock data from log client
+        var mockData = []
+        // build log client that will create mock data
+        var mockLogClient = new MockLogClient({
+            log: function (type, data) {
+                // clone data so that it is not modified after log
+                mockData.push(_.cloneDeep({type: type, data: data}))
+            }
+        })
+        // reset global singleton data
+        immutable.reset().strictArgs(false).logClient(mockLogClient)
+        // apply automock
+        automock.reset().mock()
+        // flags set when methods are called
+        var barCalled = false
+        var fooCalled = false
+        // create FooModule
+        var fooModule = immutable.module('FooModule', {
+            bar: function (args) {
+                barCalled = true
+                return Promise.resolve(true)
+            },
+            foo: function (args) {
+                fooCalled = true
+                return fooModule.bar(args)
+            },
+        })
+        // call method once to build mock data
+        return fooModule.foo().then(function (res) {
+            // reset method called flags
+            barCalled = false
+            fooCalled = false
+            // load mock data from first call to mock subsequent calls
+            automock.loadMock(mockData)
+            // call module a second time which should now have data mocked
+            return fooModule.foo()
+        })
+        // test results of mocked call
+        .then(function (res) {
+            // test that response is correct
+            assert.strictEqual(res, true)
+            // test that neither original method was called
+            assert.strictEqual(barCalled, false)
+            assert.strictEqual(fooCalled, false)
+        })
+    })
+
+    it('should not use mock when automock flag on session set to false', function () {
+        // reset global singleton data
+        immutable.reset().strictArgs(false)
+        // reset and apply automock
+        automock.reset().mock()
+        // load mock data from file
+        automock.loadMockFromFile(__dirname+'/../mock/mock-data-multi.json')
+        // flags set when methods are called
+        var barCalled = false
+        var fooCalled = false
+        // create FooModule
+        var fooModule = immutable.module('FooModule', {
+            bar: function (args) {
+                barCalled = true
+                return Promise.resolve(true)
+            },
+            foo: function (args) {
+                fooCalled = true
+                return fooModule.bar(args)
+            },
+        })
+        // call method which should be mocked
+        return fooModule.foo({
+            session: {
+                automock: false
+            }
+        })
+        // test results of mocked call
+        .then(function (res) {
+            // test that response is correct
+            assert.strictEqual(res, true)
+            // test that foo was called but bar was mocked
+            assert.strictEqual(barCalled, false)
+            assert.strictEqual(fooCalled, true)
+        })
+    })
+
+})
--- test/immutable-automock.js
+++ test/immutable-automock.js
@@ -13,91 +13,6 @@ const assert = chai.assert
 
 describe('immutable-automock', function () {
 
-    it('should wrap method with automock function', function () {
-        // reset global singleton data
-        immutable.reset()
-        // apply automock
-        automock.mock()
-        // create FooModule
-        var fooModule = immutable.module('FooModule', {
-            // foo method returns valid Promise
-            foo: function (args) {
-                return Promise.resolve(true)
-            },
-        })
-        // check that foo is wrapped with automock function
-        assert.match(fooModule.foo.toString(), /automockWrapper/)
-    })
-
-    it('should call original method when no mock data is loaded', function () {
-        // reset global singleton data
-        immutable.reset().strictArgs(false)
-        // apply automock
-        automock.mock()
-        // will be set true when method called
-        var methodCalled = false
-        // create FooModule
-        var fooModule = immutable.module('FooModule', {
-            // foo method returns valid Promise
-            foo: function (args) {
-                methodCalled = true
-            },
-        })
-        // call method
-        return fooModule.foo().then(function () {
-            // check that original method was called
-            assert.isTrue(methodCalled)
-        })
-    })
-
-    it('should return mock data and not call original method when mock data loaded', function () {
-        // build mock data from log client
-        var mockData = []
-        // build log client that will create mock data
-        var mockLogClient = new MockLogClient({
-            log: function (type, data) {
-                // clone data so that it is not modified after log
-                mockData.push(_.cloneDeep({type: type, data: data}))
-            }
-        })
-        // reset global singleton data
-        immutable.reset().strictArgs(false).logClient(mockLogClient)
-        // apply automock
-        automock.mock()
-        // flags set when methods are called
-        var barCalled = false
-        var fooCalled = false
-        // create FooModule
-        var fooModule = immutable.module('FooModule', {
-            bar: function (args) {
-                barCalled = true
-                return Promise.resolve(true)
-            },
-            foo: function (args) {
-                fooCalled = true
-                return fooModule.bar(args)
-            },
-        })
-        // call method once to build mock data
-        return fooModule.foo().then(function (res) {
-            // reset method called flags
-            barCalled = false
-            fooCalled = false
-            // load mock data from first call to mock subsequent calls
-            automock.loadMock(mockData)
-            // call module a second time which should now have data mocked
-            return fooModule.foo()
-        })
-        // test results of mocked call
-        .then(function (res) {
-            // test that response is correct
-            assert.strictEqual(res, true)
-            // test that neither original method was called
-            assert.strictEqual(barCalled, false)
-            assert.strictEqual(fooCalled, false)
-        })
-    })
-
     it('should load single line array of mock data from file', function () {
         // reset global singleton data
         immutable.reset().strictArgs(false)
@@ -199,44 +114,6 @@ describe('immutable-automock', function () {
             assert.strictEqual(res, true)
             // test that original method was called
             assert.strictEqual(barCalled, true)
-            assert.strictEqual(fooCalled, true)
-        })
-    })
-
-
-    it('should not use mock when automock flag on session set to false', function () {
-        // reset global singleton data
-        immutable.reset().strictArgs(false)
-        // reset and apply automock
-        automock.reset().mock()
-        // load mock data from file
-        automock.loadMockFromFile(__dirname+'/../mock/mock-data-multi.json')
-        // flags set when methods are called
-        var barCalled = false
-        var fooCalled = false
-        // create FooModule
-        var fooModule = immutable.module('FooModule', {
-            bar: function (args) {
-                barCalled = true
-                return Promise.resolve(true)
-            },
-            foo: function (args) {
-                fooCalled = true
-                return fooModule.bar(args)
-            },
-        })
-        // call method which should be mocked
-        return fooModule.foo({
-            session: {
-                automock: false
-            }
-        })
-        // test results of mocked call
-        .then(function (res) {
-            // test that response is correct
-            assert.strictEqual(res, true)
-            // test that foo was called but bar was mocked
-            assert.strictEqual(barCalled, false)
             assert.strictEqual(fooCalled, true)
         })
     })
