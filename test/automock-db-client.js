ce21d0a763b1aa53c0073632836eb647bcbaaf5c
Ersun Warncke <ersun.warncke@gmail.com>
2016-12-06 10:54:32 -0400

    automock db-client and prevent mock pollution

---
+++ test/automock-db-client.js
@@ -0,0 +1,131 @@
+'use strict'
+
+// load automock first
+const automock = require('../lib/immutable-automock')
+
+const ImmutableDatabaseMariaSQL = require('immutable-database-mariasql')
+const MockLogClient = require('../mock/mock-log-client')
+const Promise = require('bluebird')
+const _ = require('lodash')
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+
+chai.use(chaiAsPromised)
+const assert = chai.assert
+
+const dbHost = process.env.DB_HOST || 'localhost'
+const dbName = process.env.DB_NAME || 'test'
+const dbPass = process.env.DB_PASS || ''
+const dbUser = process.env.DB_USER || 'root'
+
+// use the same params for all connections
+const connectionParams = {
+    charset: 'utf8',
+    db: dbName,
+    host: dbHost,
+    password: dbPass,
+    user: dbUser,
+}
+
+describe('immutable-automock: db-client', function () {
+
+    beforeEach(function () {
+        // reset mock data
+        automock.reset()
+    })
+
+    it('should mock db query', function () {
+        // build mock data from log client
+        var mockData = []
+        // capture original response to test against mock response
+        var origRes
+        // build log client that will create mock data
+        var mockLogClient = new MockLogClient({
+            log: function (type, data) {
+                // skip dbConnection logs
+                if (type === 'dbConnection') {
+                    return
+                }
+                // clone data so that it is not modified after log
+                mockData.push(_.cloneDeep({type: type, data: data}))
+            }
+        })
+        // create new connection
+        var db = new ImmutableDatabaseMariaSQL(connectionParams, {
+            logClient: mockLogClient,
+        })
+        // do query
+        return db.query('SELECT CURRENT_TIMESTAMP() AS time')
+        // capture response and load mock data
+        .then(function (res) {
+            // capture original response to test against mock response
+            origRes = res
+            // load mock data
+            automock.loadMock(mockData)
+        })
+        // wait 2 seconds so actual query would return different time
+        .then(function () {
+            return Promise.delay(1000)
+        })
+        // do query again which should return mocked value
+        .then(function () {
+            return db.query('SELECT CURRENT_TIMESTAMP() AS time')
+        })
+        // test response
+        .then(function (res) {
+            // mock response should be same as original response
+            assert.deepEqual(res, origRes)
+        })
+        // close database connection
+        .finally(function () {
+            db.close()
+        })
+    })
+
+    it('should mock db query error', function () {
+        // build mock data from log client
+        var mockData = []
+        // capture original response to test against mock response
+        var origRes
+        // build log client that will create mock data
+        var mockLogClient = new MockLogClient({
+            log: function (type, data) {
+                // skip dbConnection logs
+                if (type === 'dbConnection') {
+                    return
+                }
+                // clone data so that it is not modified after log
+                mockData.push(_.cloneDeep({type: type, data: data}))
+            }
+        })
+        // create new connection
+        var db = new ImmutableDatabaseMariaSQL(connectionParams, {
+            logClient: mockLogClient,
+        })
+        // do bad query that will result in error
+        return db.query('FOO')
+        // capture response and load mock data
+        .catch(function (res) {
+            // capture original response to test against mock response
+            origRes = res
+            // load mock data
+            automock.loadMock(mockData)
+        })
+        // do query again which should return mocked value
+        .then(function () {
+            return db.query('FOO')
+        })
+        // test response
+        .catch(function (res) {
+            // mock response should be same as original response
+            assert.strictEqual(res.code, origRes.code)
+            assert.strictEqual(res.isOperational, origRes.isOperational)
+            assert.strictEqual(res.message, origRes.message)
+        })
+        // close database connection
+        .finally(function () {
+            db.close()
+        })
+    })
+
+})
---
+++ test/db-query-stable-id.js
@@ -0,0 +1,180 @@
+'use strict'
+
+const assert = require('chai').assert
+const dbQueryStableId = require('../lib/automock/db-client').dbQueryStableId
+
+describe('immutable-automock: db query stable id', function () {
+
+    it('should generate a 128bit hex id', function () {
+        assert.match(dbQueryStableId({}), /^[0-9A-Z]{32}$/)
+    })
+
+    it('should generate different ids for queries with different query strings', function () {
+        var dbQueryData1 = {
+            query: 'FOO',
+        }
+        var dbQueryData2 = {
+            query: 'BAR',
+        }
+        assert.notStrictEqual(dbQueryStableId(dbQueryData1), dbQueryStableId(dbQueryData2))
+    })
+
+    it('should generate different ids for queries with the same query strings and different moduleCallIds', function () {
+        var dbQueryData1 = {
+            moduleCallId: 'FOO',
+            query: 'FOO',
+        }
+        var dbQueryData2 = {
+            moduleCallId: 'BAR',
+            query: 'FOO',
+        }
+        assert.notStrictEqual(dbQueryStableId(dbQueryData1), dbQueryStableId(dbQueryData2))
+    })
+
+    it('should generate different ids for queries with the same query strings and different options', function () {
+        var dbQueryData1 = {
+            options: {
+                foo: true,
+            },
+            query: 'FOO',
+        }
+        var dbQueryData2 = {
+            options: {
+                foo: false,
+            },
+            query: 'FOO',
+        }
+        assert.notStrictEqual(dbQueryStableId(dbQueryData1), dbQueryStableId(dbQueryData2))
+    })
+
+    it('should generate different ids for queries with the same query strings and different params', function () {
+        var dbQueryData1 = {
+            params: {
+                foo: true,
+            },
+            query: 'FOO',
+        }
+        var dbQueryData2 = {
+            params: {
+                foo: false,
+            },
+            query: 'FOO',
+        }
+        assert.notStrictEqual(dbQueryStableId(dbQueryData1), dbQueryStableId(dbQueryData2))
+    })
+
+    it('should generate different ids for queries with the same query strings and different requestIds', function () {
+        var dbQueryData1 = {
+            query: 'FOO',
+            requestId: 'FOO',
+        }
+        var dbQueryData2 = {
+            query: 'FOO',
+            requestId: 'BAR',
+        }
+        assert.notStrictEqual(dbQueryStableId(dbQueryData1), dbQueryStableId(dbQueryData2))
+    })
+
+    it('should generate the same id for queries with the same query strings', function () {
+        var dbQueryData1 = {
+            query: 'FOO',
+        }
+        var dbQueryData2 = {
+            query: 'FOO',
+        }
+        assert.strictEqual(dbQueryStableId(dbQueryData1), dbQueryStableId(dbQueryData2))
+    })
+
+    it('should generate the same id for queries with the same query strings and the same moduleCallIds', function () {
+        var dbQueryData1 = {
+            moduleCallId: 'FOO',
+            query: 'FOO',
+        }
+        var dbQueryData2 = {
+            moduleCallId: 'FOO',
+            query: 'FOO',
+        }
+        assert.strictEqual(dbQueryStableId(dbQueryData1), dbQueryStableId(dbQueryData2))
+    })
+
+    it('should generate the same id for queries with the same query strings and the same options', function () {
+        var dbQueryData1 = {
+            options: {
+                foo: true,
+            },
+            query: 'FOO',
+        }
+        var dbQueryData2 = {
+            options: {
+                foo: true,
+            },
+            query: 'FOO',
+        }
+        assert.strictEqual(dbQueryStableId(dbQueryData1), dbQueryStableId(dbQueryData2))
+    })
+
+    it('should generate the same id for queries with the same query strings and the same params', function () {
+        var dbQueryData1 = {
+            params: {
+                foo: true,
+            },
+            query: 'FOO',
+        }
+        var dbQueryData2 = {
+            params: {
+                foo: true,
+            },
+            query: 'FOO',
+        }
+        assert.strictEqual(dbQueryStableId(dbQueryData1), dbQueryStableId(dbQueryData2))
+    })
+
+    it('should generate the same id for queries with the same query strings and the same requestIds', function () {
+        var dbQueryData1 = {
+            query: 'FOO',
+            requestId: 'FOO',
+        }
+        var dbQueryData2 = {
+            query: 'FOO',
+            requestId: 'FOO',
+        }
+        assert.strictEqual(dbQueryStableId(dbQueryData1), dbQueryStableId(dbQueryData2))
+    })
+
+    it('should generate the same id for queries with the same query strings and different connectionIds', function () {
+        var dbQueryData1 = {
+            connectionId: 'FOO',
+            query: 'FOO',
+        }
+        var dbQueryData2 = {
+            connectionId: 'BAR',
+            query: 'FOO',
+        }
+        assert.strictEqual(dbQueryStableId(dbQueryData1), dbQueryStableId(dbQueryData2))
+    })
+
+    it('should generate the same id for queries with the same query strings and different dbQueryCreateTimes', function () {
+        var dbQueryData1 = {
+            dbQueryCreateTime: 'FOO',
+            query: 'FOO',
+        }
+        var dbQueryData2 = {
+            dbQueryCreateTime: 'BAR',
+            query: 'FOO',
+        }
+        assert.strictEqual(dbQueryStableId(dbQueryData1), dbQueryStableId(dbQueryData2))
+    })
+
+    it('should generate the same id for queries with the same query strings and different dbQueryIds', function () {
+        var dbQueryData1 = {
+            dbQueryId: 'FOO',
+            query: 'FOO',
+        }
+        var dbQueryData2 = {
+            dbQueryId: 'BAR',
+            query: 'FOO',
+        }
+        assert.strictEqual(dbQueryStableId(dbQueryData1), dbQueryStableId(dbQueryData2))
+    })
+
+})
